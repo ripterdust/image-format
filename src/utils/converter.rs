744c5ab43497559b220ba92e612dd1663c2f49b8
Bryan Arévalo <bryantello2010@hotmail.com>
2024-12-21 16:04:13 -0600

    update .gitignore to include .jpeg files and refactor pixel conversion logic in Converter

--- src/utils/converter.rs
+++ src/utils/converter.rs
@@ -1,19 +1,18 @@
-use core::panic;
-use std::{fs::OpenOptions, io::Write};
-
 use image::GenericImageView;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 pub struct Converter {
-    pub image_path: String,
+    image_path: String,
     pixels: Vec<String>,
 }
 
 impl Converter {
     pub fn new(path: String) -> Converter {
-        return Converter {
+        Converter {
             image_path: path,
             pixels: Vec::new(),
-        };
+        }
     }
 
     pub fn convert(&mut self) {
@@ -27,16 +26,14 @@ impl Converter {
         let (width, _height) = image.dimensions();
 
         for (x, _y, pixel) in image.pixels() {
-            let mut hex: String = format!(
-                "#{:02X}{:02X}{:02X}{:02X} ",
-                pixel[0], pixel[1], pixel[2], pixel[3]
-            );
+            let hex: String = format!("#{:02X}{:02X}{:02X}", pixel[0], pixel[1], pixel[2]);
 
+            // Add a newline character at the end of each row
             if x == width - 1 {
-                hex.push('\n');
+                self.pixels.push(format!("{}\n", hex));
+            } else {
+                self.pixels.push(hex);
             }
-
-            self.pixels.push(hex);
         }
 
         let mut file = match OpenOptions::new()
@@ -48,7 +45,7 @@ impl Converter {
             Err(e) => panic!("Error opening file: {}", e),
         };
 
-        if let Err(e) = file.write_all(self.pixels.join("").as_bytes()) {
+        if let Err(e) = file.write_all(self.pixels.join(" ").as_bytes()) {
             panic!("Error writing to file: {}", e);
         }
     }
